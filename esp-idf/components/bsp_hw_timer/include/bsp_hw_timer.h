edb72035a42c92e2938879baba00d465a6a58f51
odddouglas <18718048671iota@gmail.com>
2025-05-17 14:30:41 +0800

    fix: 修缮目录结构，准备补充文档。修改为每120s上报一次全属性，

---
+++ esp-idf/components/bsp_hw_timer/include/bsp_hw_timer.h
@@ -0,0 +1,15 @@
+#ifndef _BSP_HW_TIMER_
+#define _BSP_HW_TIMER_
+
+#include "main.h"
+#include "driver/gptimer.h"
+
+#define TIMER_GROUP TIMER_GROUP_0
+#define TIMER_IDX TIMER_0
+#define TIMER_INTERVAL_SEC 120 // 120 秒周期
+
+extern volatile bool report_flag;
+
+void hw_timer_init(void);
+
+#endif
